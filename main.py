fd2c1fd73cfc6923a54142a3bdf2e4c88f3ff95a
Hiteshvenu A <hivelord@servitor.tail014668.ts.net>
2026-06-27 13:09:24 +0530

    adding new files and editing main NOTE: dt not used yet

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import pygame as pg
-from pygame import Surface, display
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pg.time.Clock()
+    dt = 0.0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
     while True:
         log_state()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pg.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
